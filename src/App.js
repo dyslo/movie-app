95f51d5e753b46bd1bae3f3fbd6645ce01f69337
dyslo <auau3@kakao.com>
2022-08-14 19:02:47 +0900

    내 개인정보 숨기기 >.0

--- src/App.js
+++ src/App.js
@@ -6,26 +6,6 @@ import MovieDetail from './pages/MovieDetail';
 // import MovieList from './components/MovieList';
 
 const App = () => {
-  
-  // const [movies, setMovies] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  //   useEffect(() => {
-  //       (async () => {
-  //       const { data } = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=4a18ca6ed79dbc9e07ca2082e3a48a14');
-  //       setMovies(data.results);
-  //       setLoading(false);
-  //       })();
-  //   }, []);
-  //   return (
-  //       <div>
-  //           <h1>잉기있는 영화를 추천해드립니다</h1>
-  //             <ul>
-  //               {movies.map((movie) => {
-  //                 return <li key={movie.id}>{movie.title}</li>
-  //               })}
-  //             </ul>
-  //       </div>
-  //   );
   return (
     <BrowserRouter>
       <Routes>
